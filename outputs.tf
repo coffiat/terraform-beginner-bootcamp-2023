39da339d87b3e7b05e7f0bb4e40807622a79c816
Coffi Tinh <107901990+coffiat@users.noreply.github.com>
2023-10-02 03:52:27 -0400

    # 31 enable static website hosting index and error file via terraform. Correct mistake in my prvious journal as weel (#32)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "bucket name for our static website"
   value = module.terrahouse-aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  description = "s3 static website hosting endpoint"
+  value = module.terrahouse-aws.website_endpoint
+}
--- variable.tf
+++ variable.tf
@@ -6,5 +6,12 @@ variable "bucket_name" {
 type        = string
 }
 
+variable "index_html_filepath" {
+  type        = string
+}
+
+variable "error_html_filepath" {
+  type        = string
+}
 
 # The regex pattern used in the validation condition checks for a standard UUID format.
