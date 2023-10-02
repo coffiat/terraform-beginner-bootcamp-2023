b20ba9742c137321efe74a3f0c7ac7cd1e058b2f
Coffi Tinh <107901990+coffiat@users.noreply.github.com>
2023-10-01 23:33:49 -0400

    # 29 refactor s3 bucket into the new terrahouse_aws_modules (#30)

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
+terraform {
+#    backend "remote" {
+#      hostname = "app.terraform.io"
+#      organization = "DSG"
+#    }
+#   workspaces {
+#        name = "terra-house-1"
+#}
+# cloud {
+#    organization = "DSG-PRODUCTION"
+#    workspaces {
+#        name = "terra-house-1"
+#    }
+#}
 
+}
 
- # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-
-    # Bucket Naming Rules
-    # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid       = var.user_uuid
-    
-  }
+module "terrahouse-aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
--- providers.tf
+++ providers.tf
@@ -1,16 +1,2 @@
-terraform {
-    required_providers {
-            aws = {
-        source = "hashicorp/aws"
-        version = "5.17.0"
-    }
-    }
-}
 
-provider "aws" {
-# Configuration options
-}
 
-provider "random" {
-# Configuration options
-}
