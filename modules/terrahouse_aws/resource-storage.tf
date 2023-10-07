ca763711751945d04eaec0784edd6778fa8945c3
coffiat <coffi.tinhoueto@gmail.com>
2023-10-07 20:26:41 +0000

    #36 only upload index.html if a content version has changed

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -31,6 +31,10 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
     # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
     # etag = "${md5(file("path/to/file"))}"
     etag = filemd5(var.index_html_filepath)
+    lifecycle {
+        replace_triggered_by = [ terraform_data.content.output ]
+    ignore_changes = [ etag ]
+    }
 
 }
 
@@ -43,6 +47,9 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
     content_type = "test/html"
 
 etag = filemd5(var.error_html_filepath)
+#lifecycle {
+#    ignore_changes = [ etag ]
+#        }
     }
 
     resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -66,4 +73,9 @@ etag = filemd5(var.error_html_filepath)
             }
         }
     })
+}
+
+
+resource "terraform_data" "content" {
+  input = var.content_version
 }
