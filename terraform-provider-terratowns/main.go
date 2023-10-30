5270611c29cd788d277ae1c30b5a22f1fe4987c4
coffiat <coffi.tinhoueto@gmail.com>
2023-10-30 15:11:38 +0000

    #48 setup skeleton for resource

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -1,46 +1,51 @@
-
 // Package Declaration: In Go, there are two types of packages: executable and non-executable.
 // The package main declaration indicates that this Go file is intended to create an executable program, not a reusable library.
 // In Go, the main package is a special package, and it's the entry point for running an executable program.
 // When you create a Go executable, it should always be in the main package.
 package main
 
-// fmt is short format, it contains functions for formated I/O.
+// fmt is short format, it contains functions for formatted I/O.
 import (
-	//"log"
+	"context"
 	"fmt"
+	"log"
 
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
-// func main(): Defines the main function, the entry point of the application.
-// when you run the program, it starts executing from this function.
-
+// func main(): Defines the main function, the entry point of the app.
+// When you run the program, it starts executing from this function.
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: Provider,
 	})
-	// format Printline
-	// Prints to standard output.
+	// Format.PrintLine
+	// Prints to standard output
 	fmt.Println("Hello, world!")
 }
 
-// in golang, a titlecase function will get exported
+type Config struct {
+	Endpoint string
+	Token    string
+	UserUuid string
+}
+
+// in golang, a titlecase function will get exported.
 func Provider() *schema.Provider {
 	var p *schema.Provider
 	p = &schema.Provider{
-
-		ResourcesMap: map[string]*schema.Resource{},
-
+		ResourcesMap: map[string]*schema.Resource{
+			"terratowns_home": Resource(),
+		},
 		DataSourcesMap: map[string]*schema.Resource{},
-
 		Schema: map[string]*schema.Schema{
-
 			"endpoint": {
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "The endpoint for the external service",
+				Description: "The endpoint for hte external service",
 			},
 			"token": {
 				Type:        schema.TypeString,
@@ -49,24 +54,103 @@ func Provider() *schema.Provider {
 				Description: "Bearer token for authorization",
 			},
 			"user_uuid": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "UUID for configuration",
-				//validateFunc: validateUUID,
-
+				Type:         schema.TypeString,
+				Required:     true,
+				Description:  "UUID for configuration",
+				ValidateFunc: validateUUID,
 			},
 		},
 	}
-	//p.ConfigureContextFunc = providerconfigure(p)
+	p.ConfigureContextFunc = providerConfigure(p)
 	return p
-
 }
 
-// func validateUUID(v interface{}, k string) (ws []string, errors []error) {
-// 	log.Print('validateUUID:start')
-// 	value:= v.(string)
-// 	if _,err = uuid.Parse(value);err !=nil {
-// 		errors = append(error, fmt.Errorf("invalid UUID format"))
-// 	}
-// 	log.Print('validateUUID:end')
-//}
+func validateUUID(v interface{}, k string) (ws []string, errors []error) {
+	log.Print("validateUUID:start")
+	value := v.(string)
+	if _, err := uuid.Parse(value); err != nil {
+		errors = append(errors, fmt.Errorf("invalid UUID format"))
+	}
+	log.Print("validateUUID:end")
+	return
+}
+
+func providerConfigure(p *schema.Provider) schema.ConfigureContextFunc {
+	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+		log.Print("providerConfigure:start")
+		config := Config{
+			Endpoint: d.Get("endpoint").(string),
+			Token:    d.Get("token").(string),
+			UserUuid: d.Get("user_uuid").(string),
+		}
+		log.Print("providerConfigure:end")
+		return &config, nil
+	}
+}
+
+func Resource() *schema.Resource {
+	log.Print("Resource:start")
+	resource := &schema.Resource{
+		CreateContext: resourceHouseCreate,
+		ReadContext:   resourceHouseRead,
+		UpdateContext: resourceHouseUpdate,
+		DeleteContext: resourceHouseDelete,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Name of home",
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Description of home",
+			},
+			"domain_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Domain name of home eg. *.cloudfront.net",
+			},
+			"town": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The town to which the home will belong to",
+			},
+			"content_version": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "The content version of the home",
+			},
+		},
+	}
+	log.Print("Resource:start")
+	return resource
+}
+
+func resourceHouseCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	var diags diag.Diagnostics
+
+	return diags
+}
+
+func resourceHouseRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	var diags diag.Diagnostics
+
+	return diags
+}
+
+func resourceHouseUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	var diags diag.Diagnostics
+
+	return diags
+}
+
+func resourceHouseDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	var diags diag.Diagnostics
+
+	return diags
+}
