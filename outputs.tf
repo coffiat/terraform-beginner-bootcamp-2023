d24a94a7d587e5d50f0493afd1dedc4dab30e15b
coffiat <coffi.tinhoueto@gmail.com>
2023-11-01 00:27:53 +0000

    # 54 test our terra home on terratowns test town

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-#output "bucket_name" {
-#  description = "bucket name for our static website"
-#  value       = module.terrahouse-aws.bucket_name
-#}
+output "bucket_name" {
+  description = "bucket name for our static website"
+  value       = module.terrahouse-aws.bucket_name
+}
 
-#output "s3_website_endpoint" {
-#  description = "s3 static website hosting endpoint"
-#  value       = module.terrahouse-aws.website_endpoint
-#}
+output "s3_website_endpoint" {
+  description = "s3 static website hosting endpoint"
+  value       = module.terrahouse-aws.website_endpoint
+}
 
 
-#output "cloudfront_url" {
-#  description = "The cloudfront Distribution Domain name"
-#  value       = module.terrahouse-aws.cloudfront_url
-#}
+output "cloudfront_url" {
+  description = "The cloudfront Distribution Domain name"
+  value       = module.terrahouse-aws.cloudfront_url
+}
